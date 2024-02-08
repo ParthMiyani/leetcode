33d2f3bb917f9008e52639dccc545152a05a0fe9
ParthMiyani <parthmiyani42@gmail.com>
2024-02-07 23:48:43 -0700

    linked list java - 020724

---
+++ linked_list/find_the_duplicate_number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // floyde tortoise and hare
+        // slow and fast pointers will meet at some point
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // find the start of the cycle
+        // reset slow2 to 0 and move both slow and slow2 by 1
+        // they will meet at the start of the cycle
+        int slow2 = 0;
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+        return slow;
+    }
+}
---
+++ linked_list/linked_list_cycle.java
@@ -0,0 +1,29 @@
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        // floyde tortoise and hare
+        // if there is a cycle, the fast and slow pointers will meet at some point
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
